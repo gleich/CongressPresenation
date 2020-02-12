5c736d30c26fc6d28e113da2447ba5ca0b4ce6e1
Matthew Gleich <matthewgleich@gmail.com>
2020-02-12 02:24:45 -0500

    Create page3.dart

---
+++ lib/pages/page3.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import './widgets/pageOutline.dart';
+import './page4.dart';
+
+class Page3 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageOutline(
+      nextPage: Page4(),
+      lines: [
+        "📝 The Senate decides the rules for debate",
+        "📭 Open rule allows amendments",
+        "📪 Closed rule restricts amendments",
+        "📉 Open rule makes it harder for the bill to pass because",
+        "\topponents can add claused to the bill that the proponents have to vote for",
+        "✅ If a bill wins in the majority in the Senate as yea it moves to the house",
+      ],
+      stepName: "3. Senate",
+      middleBoxWidth: 75.0,
+      linesFontSize: 30.0,
+    );
+  }
+}
