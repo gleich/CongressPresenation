147126d3a1dd091afaa3fd8e9cb8c78abadf61a0
Matthew Gleich <matthewgleich@gmail.com>
2020-02-12 02:24:30 -0500

    Update page1.dart

--- lib/pages/page1.dart
+++ lib/pages/page1.dart
@@ -1,12 +1,22 @@
 import 'package:flutter/material.dart';
 
 import './widgets/pageOutline.dart';
+import '../pages/page2.dart';
 
 class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageOutline(
-      body: Text("Page Two"),
+      nextPage: Page2(),
+      lines: [
+        "🥇 The Bill First Gets Introduced",
+        "🏛 A member of Congress or Senator can introduce bill",
+        "🙋‍♂️ Whoever introduced the bill is made a sponsor of that bill",
+        "➡️ The bill can then move onto the house of reps or the senate",
+      ],
+      stepName: "1. Introduction",
+      middleBoxWidth: 75.0,
+      linesFontSize: 30.0,
     );
   }
 }
